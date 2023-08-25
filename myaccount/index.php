5b492f6590dca9cbf03e911aecffb5d57c112807
Nicolas <info@inovea-conseil.com>
2023-08-25 10:37:42 +0200

    optin

--- myaccount/index.php
+++ myaccount/index.php
@@ -120,7 +120,9 @@ $formconfirm = '';
 $form = new Form($db);
 // Confirmation to delete
 if ($action == 'buyoption') {
-	$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"].'?id='.$id.'&option='.$option.'&action=confirm_buyoption', $langs->trans('BuyOptionTitle'), $langs->trans('ConfirmBuyOption'), 'confirm_buyoption', '', 0, 1);
+	$formconfirm = $form->formconfirm($'/index.php?id='.$id.'&option='.$option.'&action=confirm_buyoption', $langs->trans('BuyOptionTitle'), $langs->trans('ConfirmBuyOption'), 'confirm_buyoption', '', 0, 1);
+	print $formconfirm;
+
 }
 
 if($action=='confirm_buyoption' && !empty($option) && !empty($id)){
