560b3aa3845ed25e97ff11ed9d0f16c552002b27
IC-Vincent <vincent@inovea-conseil.com>
2023-08-30 14:38:41 +0200

    debug

--- class/sellyoursaasutils.class.php
+++ class/sellyoursaasutils.class.php
@@ -3270,8 +3270,7 @@ class SellYourSaasUtils
 		} else {
 			$listoflines = array($object);
 		}
-		print_r($listoflines);
-		exit();
+		
 		dol_syslog("* sellyoursaasRemoteAction START (remoteaction=".$remoteaction." initial email=".$email.(get_class($object) == 'Contrat' ? ' contractid='.$object->id.' contractref='.$object->ref: '')." timeout=".$timeout.")", LOG_DEBUG, 1);
 
 		// Load parent contract of the processed contract line $tmpobject
