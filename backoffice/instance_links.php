66543686a8d83f4e395250e2a923890a314b372b
IC-Vincent <vincent@inovea-conseil.com>
2023-09-06 13:35:50 +0200

    change from shell_exec to exec# Veuillez saisir le message de validation pour vos modifications. Les lignes

--- backoffice/instance_links.php
+++ backoffice/instance_links.php
@@ -410,7 +410,7 @@ if ($id > 0 && $action != 'edit' && $action != 'create') {
 			$formula = preg_replace('/BASH:/', '', $formula);
 			$formula = make_substitutions($formula, $substitarray);
 			// 'MAIN_VERSION_LAST_UPGRADE='.$confinstance->global->MAIN_VERSION_LAST_UPGRADE;
-			$resbashformula = shell_exec($formula);
+			$resbashformula = exec($formula);
 
 			if ($resbashformula) {
 				$stringofversion .= '='.$resbashformula;
