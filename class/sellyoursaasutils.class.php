6e136c4d8970b11f50224c4c7f6c226648b93fad
IC-Vincent <vincent@inovea-conseil.com>
2023-09-19 15:17:12 +0200

    debug

--- class/sellyoursaasutils.class.php
+++ class/sellyoursaasutils.class.php
@@ -4041,9 +4041,9 @@ class SellYourSaasUtils
 					if ($tmparray[0] === 'SQL') {
 						echo "currentcommentonqty before sql";
 						print '<pre>'.print_r($currentcommentonqty,1).'</pre>';
-						$currentcommentonqty = preg_replace('#User Accounts \([0-9]+\) : #', '' ,$currentcommentonqty);
-						echo "currentcommentonqty after sql";
-						print '<pre>'.print_r($currentcommentonqty,1).'</pre>';
+						$currentcommentonqty2 = preg_replace('#User Accounts \([0-9]+\) : #', '' ,$currentcommentonqty);
+						echo "currentcommentonqty2 after sql preg";
+						print '<pre>'.print_r($currentcommentonqty2,1).'</pre>';
 						$sqlformula = make_substitutions($tmparray[1], $substitarray);
 
 						//$serverdeployment = $this->getRemoteServerDeploymentIp($domainname);
@@ -4143,10 +4143,10 @@ class SellYourSaasUtils
 						}
 					} elseif ($tmparray[0] === 'BASH') {
 						echo "currentcommentonqty before bash";
-						print '<pre>'.print_r($currentcommentonqty,1).'</pre>';
-						$currentcommentonqty = preg_replace('#Gb supp: [0-9]+#', '' ,$currentcommentonqty);
-						echo "currentcommentonqty after bash";
-						print '<pre>'.print_r($currentcommentonqty,1).'</pre>';
+						print '<pre>'.print_r($currentcommentonqty2,1).'</pre>';
+						$currentcommentonqty2 = preg_replace('#Gb supp: [0-9]+#', '' ,$currentcommentonqty2);
+						echo "currentcommentonqty2 after bash";
+						print '<pre>'.print_r($currentcommentonqty2,1).'</pre>';
 						$bashformula = make_substitutions($tmparray[1], $substitarray);
 						
 						// SFTP refresh
@@ -4339,7 +4339,7 @@ class SellYourSaasUtils
 		if (!empty($contracthasbeenrefreshed) && ! $error) {
 			$contract->array_options['options_latestresupdate_date'] = dol_now();
 			if ($newcommentonqty) {
-				$contract->array_options['options_commentonqty'] = $currentcommentonqty . ' ' . $newcommentonqty;
+				$contract->array_options['options_commentonqty'] = $currentcommentonqty2 . ' ' . $newcommentonqty;
 			}
 
 			$contract->context['actionmsg'] = 'Update contract by '.getUserRemoteIP().' to set options_latestresupdate_date'.($newcommentonqty ? ' and options_commentonqty' : '');
