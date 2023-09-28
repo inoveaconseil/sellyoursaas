5624dbc02a433b7b0997a074e9c2532816e72599
Nicolas <info@inovea-conseil.com>
2023-09-28 10:15:57 +0200

    Register offer

--- myaccount/register.php
+++ myaccount/register.php
@@ -581,6 +581,7 @@ llxHeader($head, $title, '', '', 0, 0, $arrayofjs, array(), '', 'register');
 				<script>
 					$(document).ready(function(){
 						$('.offers').hover(function(){
+						alert($(this));
 							$('.offers').css('background',"url($(this).data-url2)");
 						},
 					  function(){
