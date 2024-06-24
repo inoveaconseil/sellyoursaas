ea91a5428d5beeee304a8e58dc03cdb9a07383f7
Fightingfire04 <65291552+Fightingfire04@users.noreply.github.com>
2024-06-24 11:28:05 +0200

    debug

--- myaccount/register_instance.php
+++ myaccount/register_instance.php
@@ -485,7 +485,7 @@ if ($reusecontractid) {		// When we use the "Restart deploy" after error from ac
 							exit(-40);
 						} else {
 							setEventMessages($langs->trans("ErrorTechnicalErrorOccurredPleaseContactUsByEmail", $emailtowarn), null, 'errors');
-							print_r("data" . $resultData);
+							dol_syslog($resultData)
 							header("Location: ".$newurl);
 							exit(-41);
 						}
